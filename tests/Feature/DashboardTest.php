1c9e5d9e459623d4e46f9162bd192025aaa1e556
enagsa <enagsa@gmail.com>
2019-09-06 14:41:30 +0200

    Lección 2: Comprobando la seguridad de forma automatizada

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+
+class DashboardTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_shows_the_dashboard_page_to_authenticated_users(){
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->get(route('home'))
+            ->assertStatus(200)
+            ->assertSee('Dashboard');
+    }
+
+    /** @test */
+    function it_redirects_guest_users_to_login_page(){
+        $this->get(route('home'))
+        ->assertStatus(302)
+        ->assertRedirect(route('login'));
+    }
+}
